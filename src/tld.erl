1cd088473366d15056ac6a30d81e00518b74917a
frogtown1 <mcgriff.benjamin@gmail.com>
2021-09-29 22:33:18 -0700

    Create reverse list mechanisms for tld index.

--- src/tld.erl
+++ src/tld.erl
@@ -1,22 +1,64 @@
 -module(tld).
--export([refresh/0]).
+-export([refresh/2]).
 
 -define(TMPFILE, "public_suffix_list.dat").
 
-%% ============
-%%     API
-%% ============
-refresh() ->
-    io:format("===== Currently refreshing..."),
-    os:cmd("curl https://publicsuffix.org/list/public_suffix_list.dat -o " ++ ?TMPFILE),
-    refresh(file, ?TMPFILE),
-    io:format("===== Process completed.~n").
+%-record(suffix, { rule, labels }).
 
+
+%% =============
+%%      API
+%% =============
+%refresh() ->
+%    io:format("===== Refreshing list and ruleset, please wait..."),
+%    os:cmd("curl https://publicsuffix.org/list/public_suffix_list.dat -o " ++ ?TMPFILE),
+%    refresh(file, ?TMPFILE),
+%    io:format("===== refresh() completed.~n").
+%extract().
+%domain().
+%subdomain().
+%suffix().
+
+%% =============
+%%   INTERNALS
+%% =============
 refresh(file, File) ->
-    {ok, Data}   = file:read(File),
-    ParsedBin    = binary:split(Data, <<"\n">>, [global]),
-    ContentLines = [ strip(ContentLine) || ContentLine <- ParsedBin ].
+    {ok, Data}     = file:read_file(File),  % is binary
+    DataFmt1       = binary:split(Data, <<"\n">>, [global]),
+    DataFmt2a      = [ trim_ws(E) || E <- DataFmt1,
+                                          E /= <<>>,
+                                          boolf_comment(E)
+                     ],
+    DataFmt2b      = [ binary:split(trim_ws(E), <<".">>, [global]) ||
+                           E <- DataFmt1,
+                                E /= <<>>,
+                                boolf_comment(E)
+                     ],
+    DataFmt2c      = [ lists:reverse(binary:split(trim_ws(E), <<".">>, [global])) ||
+                           E <- DataFmt1,
+                                E /= <<>>,
+                                boolf_comment(E)
+                     ],
+
+    %io:format("~p~n", [DataFmt2c]).
+    file:write_file("splittrim.txt", io_lib:fwrite("~p~n", [DataFmt2b])).
 
 
+trim_ws(<<$\s, Rest/binary>>) -> trim_ws(Rest);
+trim_ws(Bin) -> Bin.
 
+boolf_comment(A) ->
+    case A of
+        <<"//", _/binary>> -> false;
+        _              -> true
+    end.
 
+% #suffix{rule = 'ac', labels = []}.
+
+% [
+%  #suffix{rule = 'ac', labels = ['ac',..]},
+%  #suffix{rule = 'ad', labels = ['ad',..]},
+%  #suffix{rule = 'ae', labels = ['ae',..]},
+% ]
+
+% Convert as many data to atoms and numbers.
